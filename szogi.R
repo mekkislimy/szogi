e2fe7c08b8b884fe9ef1bcb272011bfdfe47947a
mmtx07 <magyarmate07@gmail.com>
2025-04-27 22:41:48 +0200

    Added a new product, added piechart.

--- szogi.R
+++ szogi.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
 sales_data <- data.frame(
-  product = c("Laptop", "Tablet", "Smartphone", "Desktop", "Monitor", "Printer"),
-  sales = c(150, 90, 230, 70, 85, 40)
+  product = c("Laptop", "Tablet", "Smartphone", "Desktop", "Monitor", "Printer", "Smartwatch"),
+  sales = c(150, 90, 230, 70, 85, 40, 65)
 )
 
 ggplot(sales_data, aes(x = product, y = sales, fill = product)) +
@@ -17,3 +17,12 @@ best_selling <- sales_data$product[which.max(sales_data$sales)]
 print(paste("A legjobban fogyó termék:", best_selling))
 
 #github oldálán írok
+
+
+sales_data$percentage <- sales_data$sales / sum(sales_data$sales) * 100
+
+ggplot(sales_data, aes(x = "", y = percentage, fill = product)) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y") +
+  labs(title = "Eladások eloszlása (%)") +
+  theme_minimal()
